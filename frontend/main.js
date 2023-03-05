ca6100855c69dcc3534d32be71b59d14e42006ee
aosora13 <79731216+aosora13@users.noreply.github.com>
2023-03-05 17:45:17 -0500

    Update main.js
    adding code

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-fetch("https://pythonvc.azurewebsites.net/api/PythonHTTPTrigger")
+fetch("https://pythonvc.azurewebsites.net/api/PythonHTTPTrigger?code=mv97jQqh8iPWy0riKoMAU3JHLwXZI6wsiTEq8q6fKp0CAzFueGOgJw==")
     .then(response => response.text())
     .then(data => {
         document.getElementById('counter').textContent = data;

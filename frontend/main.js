3d2d9500806b62beaeb2360220703ea8c88e3ac9
aosora13 <79731216+aosora13@users.noreply.github.com>
2023-03-05 22:24:46 -0500

    Update main.js
    Changes

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-fetch("https://pythonvc.azurewebsites.net/api/PythonHTTPTrigger")
+fetch("https://pythonvc.azurewebsites.net/api/PythonHttpTrigger")
     .then(response => response.text())
     .then(data => {
         document.getElementById('counter').textContent = data;

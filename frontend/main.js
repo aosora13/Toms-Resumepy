7a7b399e331d13b1a7497c358db6a9bf9b9e9e0e
aosora13 <79731216+aosora13@users.noreply.github.com>
2023-03-05 22:22:52 -0500

    Update main.js
    changed url

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-fetch("https://pythonvc.azurewebsites.net/api/PythonHTTPTrigger?code=mv97jQqh8iPWy0riKoMAU3JHLwXZI6wsiTEq8q6fKp0CAzFueGOgJw==")
+fetch("https://pythonvc.azurewebsites.net/api/PythonHTTPTrigger")
     .then(response => response.text())
     .then(data => {
         document.getElementById('counter').textContent = data;

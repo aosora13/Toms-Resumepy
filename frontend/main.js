6e84dab7f9f8ff971c42be094929298cd1609545
aosora13 <79731216+aosora13@users.noreply.github.com>
2023-03-11 10:56:24 -0500

    Update main.js
    changed id to "visitor_count"

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 fetch("https://pythonvc.azurewebsites.net/api/PythonHttpTrigger")
     .then(response => response.text())
     .then(data => {
-        document.getElementById('counter').textContent = data;
+        document.getElementById('visitor_count').textContent = data;
     });
